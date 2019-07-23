6401ab3044be02cd2084ae552c23bac6d59aadd9
porkcube <porkcube@ereet.st>
2019-07-23 16:46:56 -0700

    don't reinstall it everytime, that's silly

--- lint.sh
+++ lint.sh
@@ -6,17 +6,19 @@ set -o nounset
 
 export PATH=$PATH:/usr/local/bin
 
+if ! which hadolint &>/dev/null; then
+    >&2 echo 'hadolint command not found'
+fi
 if [ `uname` = "Darwin" ]; then
-    if ! which hadolint &>/dev/null; then
-        >&2 echo 'hadolint command not found'
-    fi
     if [ -z "$(command -v brew)" ]; then
         exit 1
     fi
     brew install hadolint
 elif [ `uname` = "Linux" ]; then
-    sudo curl -fsSL https://github.com/hadolint/hadolint/releases/download/v1.17.1/hadolint-Linux-x86_64 -o /usr/local/bin/hadolint
-    sudo chmod 0755 /usr/local/bin/hadolint
+    if [ -z "$(command -v hadolint)" ]; then
+        sudo curl -fsSL https://github.com/hadolint/hadolint/releases/download/v1.17.1/hadolint-Linux-x86_64 -o /usr/local/bin/hadolint
+        sudo chmod 0755 /usr/local/bin/hadolint
+    fi
 fi
 
 hadolint "$@"
