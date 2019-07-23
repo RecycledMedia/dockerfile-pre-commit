ed03c573d31eb0b7617227cf40f16e61f46a2584
porkcube <porkcube@ereet.st>
2019-07-23 16:15:46 -0700

    add Linux support

--- lint.sh
+++ lint.sh
@@ -6,12 +6,17 @@ set -o nounset
 
 export PATH=$PATH:/usr/local/bin
 
-if ! which hadolint &>/dev/null; then
-    >&2 echo 'hadolint command not found'
+if [ `uname` = "Darwin" ]; then
+    if ! which hadolint &>/dev/null; then
+        >&2 echo 'hadolint command not found'
     if [ -z "$(command -v brew)" ]; then
         exit 1
     fi
     brew install hadolint
+elif [ `uname` = "Linux" ]; then
+    sudo curl -fsSL https://github.com/hadolint/hadolint/releases/download/v1.17.1/hadolint-Linux-x86_64 -o /usr/local/bin/hadolint
+    sudo chmod 0755 /usr/local/bin/hadolint
 fi
 
 hadolint "$@"
+
